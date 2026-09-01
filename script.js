1cff9bd5dd8da99e4394e4120e8d3f78df66a480
crontroversial <nadine.geiger98@icloud.com>
2026-09-01 18:07:42 +0200

    make diagnostics interactive

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("crontroversial is running");
+
+const button = document.getElementById("diagnostics");
+
+button.addEventListener("click", function() {
+  alert("SYSTEM CHECK: somehow still running");
+});
